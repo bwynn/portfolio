eee1b4ec6e2cd11af3fa43c22415508d41160bf8
Brian Wynn <brian.wynn83@gmail.com>
2015-07-24 23:52:16 -0700

    Initial page build for my portfolio site.

---
+++ README.md
@@ -0,0 +1,2 @@
+This repo is home to my portfolio site/application. Building out a concise, dynamic portfolio that can be easily maintained."
+
---
+++ js/portfolio.js
@@ -0,0 +1,6 @@
+var prt = (function() {
+  var initModule = function($container) {
+    $container.html('<h1>Hello World!</h1>');
+  };
+  return { initModule: initModule };
+}());
