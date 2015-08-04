581599a91ffc4ce7a436e7615ba86c8f07795675
Brian Wynn <brian.wynn83@gmail.com>
2015-08-04 00:33:07 -0700

    Modified styles.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,13 @@
 
 // create a modal object to hold property and animation properties
 var modal = {
-
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  time_open_bckgd: 400,
+  time_open_window: 600,
+  time_closed: 300
 };
 
 // create an element map object to hold all values within the page.
@@ -17,29 +23,32 @@ var elem = {
   modalWindow: $(".prt-shell-modal")
 };
 
-var showModalEvent = function() {
-  elem.projectLinks.on("click", function() {
-    elem.body.scrollTop(0);
-    elem.modalBackground.show();
-    elem.prtShellModal.show();
+var showModalEvent = function(obj1, obj2, obj3, obj4) {
+  obj1.on("click", function() {
+    obj2.scrollTop(0);
+    obj3.fadeIn(modal.time_open_bckgd);
+    obj4.fadeIn(modal.time_open_window);
   });
 };
 
-// get modal window
-var modalWindow = $(".prt-shell-modal");
-
-var closeModalEvent = function(obj) {
-  obj.on("click", function() {
-    elem.modalBackground.hide();
-    elem.modalWindow.hide();
+var closeModalEvent = function(obj1, obj2, obj3) {
+  obj1.on("click", function() {
+    obj2.fadeOut(modal.time_closed, function(){
+      obj2.hide();
+      obj2.css("opacity", "1");
+    });
+    obj3.fadeOut(modal.time_closed, function() {
+      obj3.hide();
+      obj2.css("opacity", "1")
+    });
   });
 };
 
 var initModule = function() {
-  showModalEvent();
-  closeModalEvent(elem.closeButton);
-  closeModalEvent(elem.mobileClose);
-  closeModalEvent(elem.modalBackground);
+  showModalEvent(elem.projectLinks, elem.body, elem.modalBackground, elem.prtShellModal);
+  closeModalEvent(elem.closeButton, elem.modalBackground, elem.modalWindow);
+  closeModalEvent(elem.mobileClose, elem.modalBackground, elem.modalWindow);
+  closeModalEvent(elem.modalBackground, elem.modalBackground, elem.modalWindow);
 };
 
 initModule();
