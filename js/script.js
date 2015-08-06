7a216380b91c9afe87073626f8b03a1fae9bcaec
Brian Wynn <brian.wynn83@gmail.com>
2015-08-05 22:30:58 -0700

    Updated styles a bit.

--- js/script.js
+++ js/script.js
@@ -10,16 +10,43 @@ var modal = {
   bottom: 0,
   time_open_bckgd: 400,
   time_open_window: 600,
-  time_closed: 300
+  time_closed: 300,
+  content: {
+    item_link_length: -4
+  }
 };
 
 var swProject = {
   title: "Southwest Roadtrip",
   content: "A project that features big, bold retina pictures from a road trip I took with my wife in the fall of 2014. This project features a responsive gallery slider. The Southwest Roadtrip project is hand coded from the ground up, using javascript and utilizing the jQuery javascript library. Mobile touch events handled using the jQuery mobile framework.",
-  tools: ["html", "css", "javascript", "blah blah blah"],
-  browsers: ["Chrome 44", "Firefox 39", "Safari 8"],
+  tools: ["html", "css", "javascript", "next"],
+  browsers: ["Chrome 44", "Firefox 39", "Safari 8", "another"],
   links: ["http://www.brianwynndesign.com/southwest/", "https://github.com/bwynn/southwest"]
-}
+};
+
+var ndProject = {
+  title: "Norgren Design",
+  content: "Description of the norgren design project scope",
+  tools: ["html", "css", "javascript/jquery", "uriAnchor"],
+  browsers: ["Chrome 44", "Firefox 39", "Safari 8", "Opera"],
+  links: ["http://www.norgrendesign.com", "https://github.com/bwynn/norgren-design"]
+};
+
+var wavesProject = {
+  title: "Waves",
+  content: "Description of the waves project, api call, json content, etc.",
+  tools: ["html", "css", "javascript/jquery", "json", "ajax"],
+  browsers: ["Chrome 44", "Firefox 39", "Safari 8", "IE11"],
+  links: ["http://www.brianwynndesign.com/waves/", "https://github.com/bwynn/waves_js/"]
+};
+
+var sandboxProject = {
+  title: "Sandbox",
+  content: "A memory game, a flexbox layout project, gear inch calculator",
+  tools: ["html", "css", "javascript", "bootstrap", "ajax"],
+  browsers: ["Chrome 44", "Firefox 39", "Safari 8"],
+  links: ["http://www.brianwynndesign.com/sandbox/", "https://github.com/bwynn/sandbox/"]
+};
 
 // create an element map object to hold all values within the page.
 var elem = {
@@ -33,7 +60,7 @@ var elem = {
   modalTitle: $(".modal-header"),
   modalCopy: $(".modal-copy"),
   modalTools: $(".tools li"),
-  modalBrowsers: $(".tools li"),
+  modalBrowsers: $(".browsers li"),
   modalLinks: $(".project-links li")
 };
 // ------------------------- END PROJECT SCOPE VARIABLES -------------------- */
@@ -61,12 +88,31 @@ var modalBuilder = function( obj ) {
   });
 };
 
+var projectSwitcher = function( elem ) {
+  switch ( elem.attr("class") ) {
+    case "prt-shell-prj-1":
+      // Modal content propagation
+      modalBuilder(wavesProject);
+      break;
+    case "prt-shell-prj-2":
+      modalBuilder(ndProject);
+      break;
+    case "prt-shell-prj-3":
+      modalBuilder(swProject);
+      break;
+    case "prt-shell-prj-4":
+      modalBuilder(sandboxProject);
+      break;
+  }
+};
+
 // ------------------------- END DOM METHODS -------------------------------- */
 
 // ------------------------- BEGIN EVENT HANDLERS --------------------------- */
 // This function displays the modal window when a project tile has been clicked on
 var showModalEvent = function(obj1, obj2, obj3, obj4) {
   obj1.on("click", function() {
+    projectSwitcher( $(this) );
     obj2.scrollTop(0);
     obj3.fadeIn(modal.time_open_bckgd, function(){
       obj3.css("opacity", 0.85);
@@ -103,9 +149,6 @@ var initModule = function() {
   closeModalEvent(elem.closeButton, elem.modalBackground, elem.modalWindow);
   closeModalEvent(elem.mobileClose, elem.modalBackground, elem.modalWindow);
   closeModalEvent(elem.modalBackground, elem.modalBackground, elem.modalWindow);
-
-  // Modal content propagation
-  modalBuilder(swProject);
 };
 // ------------------------- END PUBLIC METHODS ----------------------------- */
 
