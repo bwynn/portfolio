5f2938aea98fb50268aee5d1d9a95c4a0b0dc70b
Brian Wynn <brian.wynn83@gmail.com>
2015-08-19 00:23:03 -0700

    Added images for the responsive presentation for waves and sandbox project.

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@
   };
 
   var swProject = {
+    id: "southwest-modal-img",
     title: "Southwest Roadtrip",
     content: "A project that features big, bold retina pictures from a road trip I took with my wife in the fall of 2014. This project features a responsive gallery slider. The Southwest Roadtrip project is hand coded from the ground up, using javascript and utilizing the jQuery javascript library. Mobile touch events handled using the jQuery mobile framework.",
     tools: ["html", "css", "javascript", "next"],
@@ -19,6 +20,7 @@
   };
 
   var ndProject = {
+    id: "nd-modal-img",
     title: "Norgren Design",
     content: "A scalable portfolio site built for a Mechanical Engineering venture by Erik Norgren. Erik is recruiting for his design firm, and wanted the ability to engage potential business partners using a connect feature. The page's content is being propagated using json, which is being passed down into a single page application built using javascript and jquery. Erik is a good friend of mine, and he wants the ability to add features to the site, so I've set this project up with easy updating via feature modules. This site is fully responsive.",
     tools: ["html", "css", "javascript/jquery", "uriAnchor"],
@@ -27,6 +29,7 @@
   };
 
   var wavesProject = {
+    id: "waves-modal-img",
     title: "Waves",
     content: "An ongoing personal project, this single page application gathers weather data from the WorldWeatherOnline API and passes it into the page. You can check the waves at 3 of the most consistent waves in California, as well as your current weather conditions. This app is built in jQuery/js as a single page application.",
     tools: ["ajax", "json", "js/jquery", "css"],
@@ -35,6 +38,7 @@
   };
 
   var sandboxProject = {
+    id: "sandbox-modal-img",
     title: "Sandbox",
     content: "A working development sandbox featuring smaller projects. The Memory Game is a small memory game written in js/jQuery. The Gear Inch Calculator utilizes Twitter's Bootstrap css framework, and is written using js/jQuery. The Flexible Layout project is a template layout using css3's flexbox. I'm big into responsive design and the power that flexbox offers over traditional floats and relative widths makes for quick and sleek layouts, using minimal code. The Style Library is a css library that I've been working on, using a grid-template similar to Bootstrap. I'm using this library on some of my upcoming projects, but a sneak peek can be seen here.",
     tools: ["html", "css", "javascript/jQuery", "bootstrap"],
@@ -58,16 +62,17 @@
     modalCopy: $(".modal-copy"),
     modalTools: $(".tools li"),
     modalBrowsers: $(".browsers li"),
-    modalLinks: $(".project-links li")
+    modalLinks: $(".project-links li"),
+    modalImage: $(".modal-featured-project > figure")
   };
   // ------------------------- END PROJECT SCOPE VARIABLES -------------------- */
 
   // ------------------------- BEGIN UTILITY METHODS -------------------------- */
   var scrollToTop = function( obj ) {
-    // scroll to top of page
-    obj.scrollTop(0);
-    // firefox & ie fix
-    elem.html.animate({ scrollTop: 0 });
+      // scroll to top of page
+      obj.scrollTop(0);
+      // firefox & ie fix
+      elem.html.animate({ scrollTop: 0 });
   };
 
   // ------------------------- END UTILITY METHODS ---------------------------- */
@@ -90,6 +95,10 @@
       var anchor = $(this).children();
 
       anchor.attr("href", obj.links[i]);
+    });
+    // loop through class assignments
+    elem.modalImage.each(function(i) {
+      $(this).attr("id", obj.id);
     });
   };
 
@@ -162,6 +171,6 @@
   };
   // ------------------------- END PUBLIC METHODS ----------------------------- */
 
-  initModule();
-  //return initModule();
+  //initModule();
+  return initModule();
 }());
