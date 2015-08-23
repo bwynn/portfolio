b6bbd6fb32a0019a88e1ee9bea8ce55bc04e4dc1
Brian Wynn <brian.wynn83@gmail.com>
2015-08-23 16:37:06 -0700

    Modified project specifications and details.

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@
     id: "southwest-modal-img",
     title: "Southwest Roadtrip",
     content: "A project that features big, bold retina pictures from a road trip I took with my wife in the fall of 2014. This project features a responsive gallery slider. The Southwest Roadtrip project is hand coded from the ground up, using javascript and utilizing the jQuery javascript library. Mobile touch events handled using the jQuery mobile framework.",
-    tools: ["html", "css", "javascript", "next"],
+    tools: ["html", "css", "js/jQuery"],
     browsers: ["Chrome 44/Mac OS X 10.10", "Firefox 39/Mac OS X 10.10", "Safari 8/Mac OS X 10.10", "IE 11/Win 8.1"],
     links: ["http://www.brianwynndesign.com/sites/southwest/", "https://github.com/bwynn/southwest"]
   };
@@ -23,7 +23,7 @@
     id: "nd-modal-img",
     title: "Norgren Design",
     content: "A scalable portfolio site built for a Mechanical Engineering venture by Erik Norgren. Erik is recruiting for his design firm, and wanted the ability to engage potential business partners using a connect feature. The page's content is being propagated using json, which is being passed down into a single page application built using javascript and jquery. Erik is a good friend of mine, and he wants the ability to add features to the site, so I've set this project up with easy updating via feature modules. This site is fully responsive.",
-    tools: ["html", "css", "javascript/jquery", "uriAnchor"],
+    tools: ["html", "css", "javascript/jQuery"],
     browsers: ["Chrome 44/Mac OS X 10.10", "Firefox 39/Mac OS X 10.10", "Safari 8/Mac OS X 10.10", "IE 11/Win 8.1"],
     links: ["http://www.brianwynndesign.com/sites/norgrendesign/", "https://github.com/bwynn/Norgren-Designs"]
   };
@@ -32,7 +32,7 @@
     id: "waves-modal-img",
     title: "Waves",
     content: "An ongoing personal project, this single page application gathers weather data from the WorldWeatherOnline API and passes it into the page. You can check the waves at 3 of the most consistent waves in California, as well as your current weather conditions. This app is built in jQuery/js as a single page application.",
-    tools: ["ajax", "json", "js/jquery", "css"],
+    tools: ["AJAX", "JSON", "js/jQuery", "css"],
     browsers: ["Chrome 44", "Firefox 39", "Safari 8", "IE 11"],
     links: ["http://www.brianwynndesign.com/sites/waves/", "https://github.com/bwynn/waves_js/"]
   };
@@ -40,8 +40,8 @@
   var sandboxProject = {
     id: "sandbox-modal-img",
     title: "Sandbox",
-    content: "A working development sandbox featuring smaller projects. The Memory Game is a small memory game written in js/jQuery. The Gear Inch Calculator utilizes Twitter's Bootstrap css framework, and is written using js/jQuery. The Flexible Layout project is a template layout using css3's flexbox. I'm big into responsive design and the power that flexbox offers over traditional floats and relative widths makes for quick and sleek layouts, using minimal code. The Style Library is a css library that I've been working on, using a grid-template similar to Bootstrap. I'm using this library on some of my upcoming projects, but a sneak peek can be seen here.",
-    tools: ["html", "css", "javascript/jQuery", "bootstrap"],
+    content: "A working development sandbox featuring smaller projects. The Memory Game is a small memory game written in js/jQuery. The Gear Inch Calculator utilizes Twitter's Bootstrap css framework, and is written using js/jQuery. The Flexible Layout project is a template layout using css3's flexbox. I'm big into responsive design and the power that flexbox offers over traditional floats and relative widths makes for quick and sleek layouts, using minimal code. The Clip Path project is intended as a working example of the clip-path property in css used as a css navigation tool.",
+    tools: ["html", "css", "javascript/jQuery", "Bootstrap"],
     browsers: ["Chrome 44", "Firefox 39", "Safari 8"],
     links: ["http://www.brianwynndesign.com/sites/sandbox/", "https://github.com/bwynn/sandbox/"]
   };
