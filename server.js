7dc1a3c4edd20c3950112ea016aa63108e698bb4
Brian Wynn <brian.wynn83@gmail.com>
2016-11-30 11:51:33 -0800

    Modified portfolio to run on a node server, to be hosted via heroku. Setting up in progress.

---
+++ server.js
@@ -0,0 +1,27 @@
+// dependencies
+const express = require('express'),
+      app = express(),
+      bodyParser = require('body-parser'),
+      morgan = require('morgan'),
+      path = require('path');
+
+// app configuration
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
+// static site route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + './public/index.html'));
+});
+
+app.listen(port);
+
+console.log("Server running on port " + port);
+
+exports = module.exports = app; 
